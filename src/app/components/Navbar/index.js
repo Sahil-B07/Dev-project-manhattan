4f09993405cd0762155c0c1b69427e3daa6ae0cf
Sahil-B07 <objectorientedguy23@gmail.com>
2023-09-02 19:51:32 +0530

    update

--- src/app/components/Navbar/index.js
+++ src/app/components/Navbar/index.js
@@ -1,27 +1,49 @@
-import Link from 'next/link'
-import { useState } from 'react'
+"use client";
 
+import Link from "next/link";
+import { useState } from "react";
 
 const Navbar = () => {
-
-  const [activeLink, setactiveLink] = useState('home')
-  const handleClick = (e)=>{
-    setactiveLink(e.current.value)
-  }
+  const [activeLink, setactiveLink] = useState("home");
+  const handleClick = (e) => {
+    setactiveLink(e.current.value);
+  };
 
   return (
     <>
-    <div className="opacity-50 h-20 w-screen z-50 top-0 left-0 bg-gradient-to-b from-black from-5% to-[#00000001] to-95% fixed"></div>
-    <div className="w-screen backdrop-blur-[2px] text-neutral-300 p-2 flex-col md:flex-row justify-center items-center top-0 z-50 fixed">
-
-      <nav className="md:ml-auto md:mr-auto flex text-base justify-center items-center mt-2">
-        <Link href={'/'} value='home' onClick={handleClick} className={`mx-3 cursor-pointer ${activeLink==='home'?'active':null}`}>Home</Link>
-        <Link href={'/library'} value='library' onClick={handleClick} className={`mx-3 cursor-pointer`}>Library</Link>
-        <Link href={'/about'} value='about' onClick={handleClick} className={`mx-3 cursor-pointer`}>About</Link>
-      </nav>
-    </div>
+      <div className="opacity-50 h-20 w-screen z-50 top-0 left-0 bg-gradient-to-b from-black from-5% to-[#00000001] to-95% fixed"></div>
+      <div className="w-screen backdrop-blur-[2px] text-neutral-300 p-2 flex-col md:flex-row justify-center items-center top-0 z-50 fixed">
+        <nav className="md:ml-auto md:mr-auto flex text-base justify-center items-center mt-2">
+          <Link
+            href={"/"}
+            value="home"
+            onClick={handleClick}
+            className={`mx-3 cursor-pointer ${
+              activeLink === "home" ? "active" : null
+            }`}
+          >
+            Home
+          </Link>
+          <Link
+            href={"/library"}
+            value="library"
+            onClick={handleClick}
+            className={`mx-3 cursor-pointer`}
+          >
+            Library
+          </Link>
+          <Link
+            href={"/about"}
+            value="about"
+            onClick={handleClick}
+            className={`mx-3 cursor-pointer`}
+          >
+            About
+          </Link>
+        </nav>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
