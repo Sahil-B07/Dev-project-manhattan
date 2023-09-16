88998f3a7e12c5fe28fa294fac9f9853ceea5aa2
Sahil-B07 <objectorientedguy23@gmail.com>
2023-09-16 19:39:41 +0530

    trial_f

--- build.sh
+++ build.sh
@@ -12,4 +12,4 @@ python -m pip install -r requirements.txt
 
 # Collect static files
 echo "Collecting static files..."
-python manage.py collectstatic --noinput --clear
+python books_backend/manage.py runserver
